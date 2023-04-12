c23d3bc009a2907f8dea79a5bb4593e1476e230a
Aman Shah <shahaman0011@gmail.com>
2023-04-12 07:15:52 +0530

    knowledge has been delivered

--- src/main/java/com/payment/payment/feign/PaymentClient.java
+++ src/main/java/com/payment/payment/feign/PaymentClient.java
@@ -4,12 +4,11 @@ import com.payment.payment.model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "Beginner-api",url="localhost:8051/api")
+@FeignClient(name = "Customer-api",url="localhost:8051/api")
 public interface PaymentClient {
     @PostMapping(value = "/enter")
     String enterTheCustomer(@RequestBody Customer customer);
 
     @GetMapping(value = "/fetch/{accountNumber}")
     Customer getTheCustomer(@PathVariable("accountNumber") String accountNumber);
-
 }
